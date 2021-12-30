f7a869942a183d5913aa30e9fb9205b503b33994
MeshachDev <62976486+MeshachDev@users.noreply.github.com>
2021-12-30 10:32:31 +0530

    all files uploaded and backed UP!

---
+++ learn/app.js
@@ -0,0 +1,10 @@
+const header = document.getElementById("header");
+console.log(window.scrollX)
+// if(window.scrollX == 0)
+// {
+//     header.style.display="none"
+//     header.style.backgroundColor = "red"
+// }
+// else{
+// header.style.backgroundColor = "black"
+// }
